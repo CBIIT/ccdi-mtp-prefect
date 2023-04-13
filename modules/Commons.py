065563a353be2c75fc8a043c30e0c476df99f90d
Chen <jonkiky@gmail.com>
2023-04-13 12:15:03 -0400

    add Copy file from s3 to local function

---
+++ modules/Commons.py
@@ -0,0 +1,92 @@
+import logging.config
+import csv
+import json
+import os
+import sys
+import hashlib
+from modules.Logger import logger
+
+logger = logging.getLogger(__name__)
+
+
+'''
+check if dict has key or not,return key value if key exists, otherwise return default value
+input : dict
+        key  
+        default value
+output: key value or default value
+'''
+def check_dict_key(my_dict, key, default_value):
+    if key not in my_dict:
+        return default_value
+    else:
+        return my_dict[key]
+
+
+'''
+Delete file from local machine by given file path
+
+input:  file path
+output :N/A
+'''
+
+def remove_file(file_path):
+    try:
+        os.remove(file_path)
+        logger.info(f"{file_path} deleted successfully")
+    except OSError as e:
+        logger.error(f"Error deleting file: {e.filename} - {e.strerror}")
+
+
+def validate_checksum(filename, type,expected_checksum):
+    with open(filename, "rb") as f:
+        # create a new MD5 hash object
+        if type=="md5":
+            alg = hashlib.md5()
+        if type == "SHA-1":
+            alg = hashlib.sha1()
+        if type == "SHA-2":
+            alg = hashlib.sha256()
+
+        # read the file in chunks to avoid loading the whole file into memory at once
+        chunk_size = 1024 * 1024  # 1 MB
+        chunk = f.read(chunk_size)
+        while chunk:
+            alg.update(chunk)
+            chunk = f.read(chunk_size)
+
+    # compare the calculated checksum with the expected one
+    if alg.hexdigest() == expected_checksum:
+        return True
+    return  False
+
+
+def read_csv_to_dict(path):
+    with open(path, mode='r') as csvfile:
+        reader = csv.reader(csvfile)
+        return list(reader)
+
+
+def create_directory_if_not_found(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+def read_json_with_multiple_Obj(path):
+    jsonList = []
+    with open(path) as f:
+        for jsonObj in f:
+            jsonList.append(json.loads(jsonObj))
+    return jsonList
+
+
+# (source): https://gist.github.com/vladignatyev/06860ec2040cb497f0f3
+def progress(count, total, status=''):
+    bar_len = 100
+    filled_len = int(round(bar_len * count / float(total)))
+
+    percents = round(100.0 * count / float(total), 1)
+    bar = '=' * filled_len + '-' * (bar_len - filled_len)
+
+    sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
+    sys.stdout.flush()
---
+++ modules/cfg.py
@@ -0,0 +1,46 @@
+import os, sys, errno
+import csv
+import configargparse
+import logging
+
+logger = logging.getLogger(__name__)
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+"""
+This will create a singleton argument parser that is appropriately configured
+with the various command line, environment, and ini/yaml file options.
+"""
+
+def setup_parser():
+    p = configargparse.get_argument_parser(config_file_parser_class=configargparse.YAMLConfigFileParser)
+    p.description = 'PPDC data pipleline'
+
+    # argument to read config file
+    p.add('-c', '--config', is_config_file=True,
+        env_var="PIS_CONFIG", help='path to config file (YAML)')
+    return p
+
+
+def get_args():
+    p = configargparse.get_argument_parser()
+    #dont use parse_args because that will error
+    #if there are extra arguments e.g. for plugins
+    args = p.parse_known_args()[0]
+
+    #output all configuration values, useful for debugging
+    #p.print_values()
+
+    return args
+
+
+def get_input_file(input_filename, default_name_file):
+    if input_filename is None:
+        return default_name_file
+
+    # check the file exists
+    if not os.path.isfile(input_filename):
+        raise IOError(
+            errno.ENOENT, os.strerror(errno.ENOENT), ' The input file does not exists: %s' % input_filename)
+    else:
+        return input_filename
