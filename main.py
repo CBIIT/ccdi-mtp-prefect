2c2f5f408a32794e16dc287f4ee1543d2077d957
Chen <jonkiky@gmail.com>
2023-04-17 17:03:45 -0400

    update the logging system to use prefect log

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import requests
+import yaml
 import modules.S3ToLocal as S3ToLocal
-import modules.cfg as cfg
-from modules.YAMLReader import YAMLReader
 from modules.Logger import logger
 
 def execution(step, config):
@@ -10,11 +10,17 @@ def execution(step, config):
         logger.error("Unknown step: %s", step)
 
 
-def main():
-    cfg.setup_parser()
-    args = cfg.get_args()
-    yaml = YAMLReader(args.config)
-    yaml_dict = yaml.read_yaml()
+def main(url):
+
+
+    url = "https://example.com/example.yaml"
+    response = requests.get(url)
+    yaml_dict = yaml.safe_load(response.content)
+    #
+    # cfg.setup_parser()
+    # args = cfg.get_args()
+    # yaml = YAMLReader(args.config)
+    # yaml_dict = yaml.read_yaml()
     for step in yaml_dict.steps:
         logger.info("Request to run step: %s", step)
         if(yaml_dict[step] is None):
--- modules/Logger.py
+++
@@ -1,5 +0,0 @@
-import logging.config
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-
